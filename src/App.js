454cf59a0e6ab890f881395de2cc46ff9f9d2909
charlesonunze <charlesonunze2@gmail.com>
2020-11-25 09:17:30 +0100

    make test pass

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Docker!!!
+          Learn react and Docker!!!
         </a>
       </header>
     </div>
